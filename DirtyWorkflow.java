91c4bee3032449600dd12df3dfce97835e1ecb94
Joilnen <joilnen@gmail.com>
2012-10-16 00:51:42 -0300

    Coloquei os botaoes ainda estao toscos mas eh soh o comeco

--- DirtyWorkflow.java
+++ DirtyWorkflow.java
@@ -1,12 +1,19 @@
 import javax.swing.JApplet;
 import javax.swing.SwingUtilities;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.Dimension;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.Container;
 
 public class DirtyWorkflow extends JApplet {
 
@@ -14,15 +21,18 @@ public class DirtyWorkflow extends JApplet {
     JMenuBar jmenuBar;
     int width = 1330, height = 650;
 
+    JButton b_proc, b_decis, b_seta;
+    FlowLayout layout;
+
     public void init() {
         resize(width, height);
-        add(new MainWindow());
         jmenuBar = new JMenuBar();
 
         jmenu_arquivo = new JMenu("Arquivo");
         jmenu_arquivo.add(new JMenuItem("Abrir"));
         jmenu_arquivo.add(new JMenuItem("Salvar"));
         jmenu_arquivo.add(new JMenuItem("Salvar Como"));
+
         /***
         JMenuItem sair = new JMenuItem("Sair");
             
@@ -49,6 +59,21 @@ public class DirtyWorkflow extends JApplet {
         jmenuBar.add(jmenu_help);
 
         setJMenuBar(jmenuBar);
+
+        add(new MainWindow());
+
+        layout = new FlowLayout();
+        addComponentsToPane(getContentPane());
+    }
+
+    public void addComponentsToPane(final Container pane) {
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.add(new JToggleButton("Processo")); 
+        p.add(new JToggleButton("Seta")); 
+        p.add(new JToggleButton("Decisao")); 
+        // p.setLayout(new BoxLayout());
+        pane.add(p, BorderLayout.WEST);
     }
 }
 
